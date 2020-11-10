86da257c49fb79118c15de9e8470c867f7569996
sarah dakkak <sarah.dakkak1@gmail.com>
2020-11-10 14:54:49 -0500

    cscs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+     # # read all restaurants
+  get '/tasks', to: 'tasks#index'
+
+  # read one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # create a task >> 2 steps
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # update a task >> 2 steps
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
