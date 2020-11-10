eb5d946f743abc090eb4653f00fba196dd0fa31d
sarah dakkak <sarah.dakkak1@gmail.com>
2020-11-10 14:58:20 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
      # # read all restaurants
-  get '/tasks', to: 'tasks#index'
+  # get '/tasks', to: 'tasks#index'
 
-  # read one task
-  get '/tasks/:id', to: 'tasks#show', as: :task
+  # # read one task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
 
-  # create a task >> 2 steps
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post '/tasks', to: 'tasks#create'
+  # # create a task >> 2 steps
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # post '/tasks', to: 'tasks#create'
 
-  # update a task >> 2 steps
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch '/tasks/:id', to: 'tasks#update'
+  # # update a task >> 2 steps
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
 
-  # delete a task
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # # delete a task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
+
 end
